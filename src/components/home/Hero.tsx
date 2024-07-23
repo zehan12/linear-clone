9100f3458da6b52f90536a028becf254104e833d
zehan12 <zehan9211@gmail.com>
2024-07-24 00:46:25 +0530

    update hero section

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -2,21 +2,29 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
     return (
-        <section className="h-full w-full bg-gradient-to-l from-black to-[#0C0515] mask-radial-faded pt-20 md:pt-40">
+        <section className="h-full w-full bg-gradient-to-l from-black to-[#0C0515] mask-radial-faded pt-24 md:pt-40">
             <div className="max-w-[1200px] mx-auto">
                 <h1 className="hidden md:block text-5xl font-semibold text-white leading-snug animate-blur-in">
                     Linear is a purpose-built tool for
                     <br /> planning and building products
                 </h1>
-                <h1 className="block md:hidden text-5xl font-semibold text-center animate-blur-in">
-                    Plan and build <br />
-                    your product
+                <h1 className="block md:hidden text-5xl font-semibold text-center animate-blur-in text-white leading-tight">
+                    Plan and
+                    <br />
+                    build your <br />
+                    product
                 </h1>
-                <div className="my-6 text-2xl font-medium text-neutral-400 animate-blur-in">
+                <div className="hidden md:block my-6 text-2xl font-medium text-neutral-400 animate-blur-in">
                     <p>Meet the system for modern software development.</p>
                     <p>Streamline issues, projects, and product roadmaps.</p>
                 </div>
-                <div className="flex items-center my-10 gap-4 animate-blur-in">
+                <div className="block md:hidden text-center my-6 text-2xl text-gray-300">
+                    <p>Linear is a purpose-build tool for</p>
+                    <p>modern product development </p>
+                    <p>Streamline issues, projects,</p>
+                    <p>and product roadmaps.</p>
+                </div>
+                <div className="flex flex-col md:flex-row items-center my-10 gap-4 animate-blur-in">
                     <Button
                         className="bg-gray-200 hover:bg-white font-[550]"
                         variant={"secondary"}
