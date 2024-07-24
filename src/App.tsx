3252c602228e0ad7e7c2dd76f2595f904eef6070
zehan12 <zehan9211@gmail.com>
2024-07-24 10:13:10 +0530

    import in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header, Footer } from "./components/common";
 import Hero from "./components/home/Hero";
+import { Partnerships } from "./components/home/Partnership";
 
 function App() {
     return (
         <>
             <Header />
             <Hero />
+            <Partnerships />
             <Footer />
         </>
     );
