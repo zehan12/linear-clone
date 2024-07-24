eb60313089c1b19ecb4a9e955288dcd0ec0a846b
zehan12 <zehan9211@gmail.com>
2024-07-24 17:28:15 +0530

    update product team component to mobile responsive

--- src/components/home/ProductTeam.tsx
+++ src/components/home/ProductTeam.tsx
@@ -1,6 +1,17 @@
 import { Plus } from "lucide-react";
+import { useState } from "react";
 
 export const ProductTeam = () => {
+    const [currentSlide, setCurrentSlide] = useState<number>(0);
+
+    const nextSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % 3);
+    };
+
+    const prevSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide - 1 + 3) % 3);
+    };
+
     return (
         <section className="w-full flex flex-col items-center bg-gradient-to-l from-black to-[#0C0515] py-20 text-white">
             <div className="flex flex-col md:flex-row justify-around md:items-center items-start px-6 md:px-0 gap-5 md:gap-0 w-full md:w-7/12">
@@ -17,67 +28,84 @@ export const ProductTeam = () => {
                     </p>
                 </div>
             </div>
-            <div className="flex gap-3 p-10 md:w-8/12 h-full">
-                <div className="relative w-2/6 px-5 pb-5 bg-black rounded-3xl group">
-                    <img
-                        className="w-full max-w-sm mx-auto h-auto rounded-b-3xl"
-                        src="/assets/home/product_team/product1.avif"
-                        alt="Product 1"
-                    />
-                    <div className="absolute w-full h-full top-0 left-0 bg-zinc-800/60 opacity-0 transition-opacity duration-300 group-hover:opacity-40 rounded-3xl"></div>
-
-                    <div className="flex items-center justify-between my-4">
-                        <h4 className="text-xl">
-                            Purpose-built for <br /> product development{" "}
-                        </h4>
-                        <div className="cursor-pointer">
-                            <Plus
-                                className="p-2 border-[1px] border-zinc-800 rounded-full transition-colors duration-300 group-hover:bg-zinc-800"
-                                size={40}
-                            />
+            <div className="hidden md:flex gap-3 p-10 md:w-8/12 h-full">
+                {[
+                    "/assets/home/product_team/product1.avif",
+                    "/assets/home/product_team/product2.avif",
+                    "/assets/home/product_team/product3.avif",
+                ].map((src, index) => (
+                    <div
+                        key={index}
+                        className="relative w-2/6 px-5 pb-5 bg-black rounded-3xl group"
+                    >
+                        <img
+                            className="w-full max-w-sm mx-auto h-auto rounded-b-3xl"
+                            src={src}
+                            alt={`Product ${index + 1}`}
+                        />
+                        <div className="absolute w-full h-full top-0 left-0 bg-zinc-800/60 opacity-0 transition-opacity duration-300 group-hover:opacity-40 rounded-3xl"></div>
+                        <div className="flex items-center justify-between my-4">
+                            <h4 className="text-xl">
+                                Purpose-built for <br /> product development{" "}
+                            </h4>
+                            <div className="cursor-pointer">
+                                <Plus
+                                    className="p-2 border-[1px] border-zinc-800 rounded-full transition-colors duration-300 group-hover:bg-zinc-800"
+                                    size={40}
+                                />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="relative w-2/6 px-5 pb-5 bg-black rounded-3xl group">
-                    <img
-                        className="w-full max-w-sm mx-auto h-auto rounded-b-3xl"
-                        src="/assets/home/product_team/product2.avif"
-                        alt="Product 2"
-                    />
-                    <div className="absolute w-full h-full top-0 left-0 bg-zinc-800/60 opacity-0 transition-opacity duration-300 group-hover:opacity-40 rounded-3xl"></div>
-
-                    <div className="flex items-center justify-between my-4">
-                        <h4 className="text-xl">
-                            Purpose-built for <br /> product development{" "}
-                        </h4>
-                        <div className="cursor-pointer">
-                            <Plus
-                                className="p-2 border-[1px] border-zinc-800 rounded-full transition-colors duration-300 group-hover:bg-zinc-800"
-                                size={40}
+                ))}
+            </div>
+            <div className="md:hidden w-full relative overflow-hidden">
+                <div
+                    className="flex transition-transform duration-500"
+                    style={{
+                        transform: `translateX(-${currentSlide * 100}%)`,
+                    }}
+                >
+                    {[
+                        "/assets/home/product_team/product1.avif",
+                        "/assets/home/product_team/product2.avif",
+                        "/assets/home/product_team/product3.avif",
+                    ].map((src, index) => (
+                        <div
+                            key={index}
+                            className="w-full flex-shrink-0 px-5 pb-5 bg-black rounded-3xl group"
+                        >
+                            <img
+                                className="w-full max-w-sm mx-auto h-auto rounded-b-3xl"
+                                src={src}
+                                alt={`Product ${index + 1}`}
                             />
+                            <div className="absolute w-full h-full top-0 left-0 bg-zinc-800/60 opacity-0 transition-opacity duration-300 group-hover:opacity-40 rounded-3xl"></div>
+                            <div className="flex items-center justify-between my-4">
+                                <h4 className="text-xl">
+                                    Purpose-built for <br /> product development{" "}
+                                </h4>
+                                <div className="cursor-pointer">
+                                    <Plus
+                                        className="p-2 border-[1px] border-zinc-800 rounded-full transition-colors duration-300 group-hover:bg-zinc-800"
+                                        size={40}
+                                    />
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
-                <div className="relative w-2/6 px-5 pb-5 bg-black rounded-3xl group">
-                    <img
-                        className="w-full max-w-sm mx-auto h-auto rounded-b-3xl"
-                        src="/assets/home/product_team/product3.avif"
-                        alt="Product 3"
-                    />
-                    <div className="absolute w-full h-full top-0 left-0 bg-zinc-800/60 opacity-0 transition-opacity duration-300 group-hover:opacity-40 rounded-3xl"></div>
-
-                    <div className="flex items-center justify-between my-4">
-                        <h4 className="text-xl">
-                            Purpose-built for <br /> product development{" "}
-                        </h4>
-                        <div className="cursor-pointer">
-                            <Plus
-                                className="p-2 border-[1px] border-zinc-800 rounded-full transition-colors duration-300 group-hover:bg-zinc-800"
-                                size={40}
-                            />
-                        </div>
-                    </div>
-                </div>
+                <button
+                    onClick={prevSlide}
+                    className="absolute left-0 top-1/2 transform -translate-y-1/2 p-2 bg-black bg-opacity-50 rounded-full text-white"
+                >
+                    {"<"}
+                </button>
+                <button
+                    onClick={nextSlide}
+                    className="absolute right-0 top-1/2 transform -translate-y-1/2 p-2 bg-black bg-opacity-50 rounded-full text-white"
+                >
+                    {">"}
+                </button>
             </div>
         </section>
     );
