311ed49fc3a5558643d7b423761b25f0141655d8
zehan12 <zehan9211@gmail.com>
2024-07-24 17:40:52 +0530

    fix issues

--- src/components/home/ProductTeam.tsx
+++ src/components/home/ProductTeam.tsx
@@ -13,9 +13,9 @@ export const ProductTeam = () => {
     };
 
     return (
-        <section className="w-full flex flex-col items-center bg-gradient-to-l from-black to-[#0C0515] py-20 text-white">
-            <div className="flex flex-col md:flex-row justify-around md:items-center items-start px-6 md:px-0 gap-5 md:gap-0 w-full md:w-7/12">
-                <h3 className="text-4xl md:text-5xl font-semibold leading-10 md:leading-snug">
+        <section className="w-full flex flex-col items-center gap-10 md:gap-0 bg-gradient-to-l from-black to-[#0C0515] py-20 text-white">
+            <div className="flex flex-col md:flex-row justify-start md:items-center items-start px-6 md:px-0 gap-5 md:gap-20 w-full md:w-8/12">
+                <h3 className="text-4xl md:text-5xl font-semibold leading-10 md:leading-snug md:ml-10 ">
                     Made for modern
                     <br /> product teams
                 </h3>
@@ -39,7 +39,7 @@ export const ProductTeam = () => {
                         className="relative w-2/6 px-5 pb-5 bg-black rounded-3xl group"
                     >
                         <img
-                            className="w-full max-w-sm mx-auto h-auto rounded-b-3xl"
+                            className="w-full max-w-sm mx-auto h-auto rounded-b-3xl md:opacity-50"
                             src={src}
                             alt={`Product ${index + 1}`}
                         />
